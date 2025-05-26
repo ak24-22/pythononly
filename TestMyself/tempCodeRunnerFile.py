c2d63db07a370b18a3f6fe66285b182624464005
AK <115095371+ak24-22@users.noreply.github.com>
2025-05-26 15:59:52 +0100

    Time Table Updated
    changed the variable from limit to i in the for loop

--- TestMyself/tempCodeRunnerFile.py
+++ TestMyself/tempCodeRunnerFile.py
@@ -5,7 +5,7 @@ limit = int(input("Enter Number You Want To Go To: "))
 print("\n")
 
 for i in range(1, limit + 1):
-    print(f"{num} x {limit} = {i*num}")
+    print(f"{num} x {i} = {i*num}")
     
 print("\n")
 
