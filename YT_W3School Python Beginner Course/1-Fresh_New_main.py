ac6ccf338a1a38fe8e0e22841c284f000b7592c6
AK <aliabdulkareem942@gmail.com>
2026-09-09 20:48:46 +0100

    Now on Working With Numbers In Python

--- YT_W3School Python Beginner Course/1-Fresh_New_main.py
+++ YT_W3School Python Beginner Course/1-Fresh_New_main.py
@@ -7,18 +7,25 @@ Batman_Speaks = 'Enough! How many more people are you going hurt?!'
 Batman_Thoughts = 'Did he really forgive? or is he planning a secret, quick counter attack...?'
 Krypt_Btrng_num = 55
 
-print(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang.\n {Sh2} said, {Batman_Speaks.upper()}")
+print(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang. {Sh2} said, {Batman_Speaks.upper()}\n{Sh3} forgave {Sh1}, but {Sh2} was skeptical suspicious, thinking to himself {Batman_Thoughts}")
 
-print(len(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang.\n {Sh2} said, {Batman_Speaks.upper()}"))
-print(len(Batman_Speaks))
+print(len(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang.{Sh2} said, {Batman_Speaks.upper()}\n{Sh3} forgave {Sh1}, but {Sh2} was skeptical suspicious, thinking to himself {Batman_Thoughts}"))
+
+phrase = "Shrek Academy"
+print(phrase.replace("Shrek", "Monster"))
 
 print(Batman_Speaks[5])
 print(Sh1[4])
 
 print(Batman_Speaks.index("H"))
 
+fav_num = 3
+print(str(fav_num) + " is my favourite number!") # use str() when concatenation
 
-print(f"{Sh3} forgave {Sh1}, but {Sh2} was skeptical suspicious, thinking to himself {Batman_Thoughts}")
-Sh3.replace("Aquaman", "Spiderman")
+print(pow(7,2))
+print(pow(12,3,2))
+
+
+
 
 
