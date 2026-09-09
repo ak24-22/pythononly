94cebc34bae3b34da2c9e8cdc8146ab1ddc9c020
AK <aliabdulkareem942@gmail.com>
2026-09-09 15:36:30 +0100

    Basics so far

---
+++ YT_W3School Python Beginner Course/1-Fresh_New_main.py
@@ -0,0 +1,24 @@
+# Python Beginner 
+
+Sh1 = "Superman"
+Sh2 = "Batman"
+Sh3 = "Aquaman"
+Batman_Speaks = 'Enough! How many more people are you going hurt?!'
+Batman_Thoughts = 'Did he really forgive? or is he planning a secret, quick counter attack...?'
+Krypt_Btrng_num = 55
+
+print(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang.\n {Sh2} said, {Batman_Speaks.upper()}")
+
+print(len(f"{Sh2} stopped {Sh1} from killing {Sh3} and innocent people by throwing {Krypt_Btrng_num} Kryptonite Batarang.\n {Sh2} said, {Batman_Speaks.upper()}"))
+print(len(Batman_Speaks))
+
+print(Batman_Speaks[5])
+print(Sh1[4])
+
+print(Batman_Speaks.index("H"))
+
+
+print(f"{Sh3} forgave {Sh1}, but {Sh2} was skeptical suspicious, thinking to himself {Batman_Thoughts}")
+Sh3.replace("Aquaman", "Spiderman")
+
+
